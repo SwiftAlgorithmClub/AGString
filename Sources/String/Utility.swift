32ade4deadfe0acfbe37109f146abe1c2d8022ef
SooHwanCho <chh0107@gmail.com>
2019-11-04 21:04:01 +0900

    add occurence feature

--- Sources/String/Utility.swift
+++ Sources/String/Utility.swift
@@ -29,4 +29,71 @@ extension String {
             }
         }
     }
+
+}
+
+extension String {
+
+    public func occurence(of str: String) -> Int {
+        let length = self.count
+        let strLength = str.count
+
+        var result = 0
+
+        // pi[i] = N의 길이가 i인 접두사의 접두사도 되고 접미사도 되는 문자열의 최대 길이
+        let partial: [Int] = str.getPartialMatch()
+
+        var begin = 0
+        var matched = 0
+
+        while begin <= length - strLength {
+            let hIndex = self.index(self.startIndex, offsetBy: begin+matched)
+            let nIndex = str.index(self.startIndex, offsetBy: matched)
+
+            if matched < strLength, self[hIndex] == str[nIndex] {
+                matched += 1
+
+                if matched == strLength {
+                    result += 1
+                }
+            } else {
+                if matched == 0 {
+                    begin += 1
+                } else {
+                    begin += (matched - partial[matched - 1])
+
+                    matched = partial[matched - 1]
+                }
+            }
+        }
+
+        return result
+    }
+
+    fileprivate func getPartialMatch() -> [Int] {
+        let length = self.count
+        var result = Array(repeating: 0, count: length)
+
+        var begin = 1
+        var matched = 0
+
+        while begin + matched < length {
+            let postfixIndex = self.index(startIndex, offsetBy: begin+matched)
+            let prefixIndex = self.index(startIndex, offsetBy: matched)
+
+            if self[prefixIndex] == self[postfixIndex] {
+                matched += 1
+                result[begin + matched - 1] = matched
+            } else {
+                if matched == 0 {
+                    begin += 1
+                } else {
+                    begin += matched - result[matched - 1]
+                    matched = result[matched - 1]
+                }
+            }
+        }
+
+        return result
+    }
 }
