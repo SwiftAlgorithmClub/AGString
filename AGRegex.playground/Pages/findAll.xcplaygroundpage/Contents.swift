102aa1d0bd58305eaa999055668658222cb6e68b
tyler <kimtaesoo188@gmail.com>
2019-11-10 17:25:15 +0900

    Add example on playgorund

--- AGRegex.playground/Pages/findAll.xcplaygroundpage/Contents.swift
+++ AGRegex.playground/Pages/findAll.xcplaygroundpage/Contents.swift
@@ -6,11 +6,14 @@ let r = try! NSRegularExpression(pattern: "ai", options: [])
 let regex = AGRegex(r)
 let str = "The rain in Spain"
 let actual = regex.findAll(str)
-let expect = [
-    AGMatch(start: 5, end: 7, base: str, groups: ["ai"]),
-    AGMatch(start: 14, end: 16, base: str, groups: ["ai"]),
-]
 
-actual == expect
+actual
 
-//: [Next](@next)
+/*:
+ ### Expects
+ 
+ * AGMatch(start: 5, end: 7, base: str, groups: ["ai"]),
+ * AGMatch(start: 14, end: 16, base: str, groups: ["ai"]),
+ */
+
+//: [Next](FirstLast)
